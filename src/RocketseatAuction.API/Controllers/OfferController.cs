69ca07100febd291e1e4b5d9de0afc1a37f4fb41
Leandro Alves <leandro_alvesbr@outlook.com>
2024-02-07 22:13:10 -0300

    Feeding the database with information and testing the simulation of an authentication token to represent a logged-in user in the project

---
+++ src/RocketseatAuction.API/Controllers/OfferController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RocketseatAuction.API.Comunication.Requests;
+using RocketseatAuction.API.Entities;
+using RocketseatAuction.API.Filters;
+using RocketseatAuction.API.UseCases.Offers.CreateOffer;
+
+namespace RocketseatAuction.API.Controllers;
+
+public class OfferController : RocketseatAuctionBaseController
+{
+    [HttpPost]
+    [Route("{itemId}")]
+    [ServiceFilter(typeof(AuthenticationUserAttribute))]
+    public IActionResult CreateOffer(
+        [FromRoute]int itemId,
+        [FromBody] RequestCreateOfferJson request,
+        [FromServices] CreateOfferUserCase useCase)        
+    {
+        var id = useCase.Execute(itemId, request);
+        return Created(string.Empty, id);
+    }
+}
--- src/RocketseatAuction.API/Program.cs
+++ src/RocketseatAuction.API/Program.cs
@@ -1,15 +1,53 @@
+using Microsoft.OpenApi.Models;
+using RocketseatAuction.API.Filters;
+using RocketseatAuction.API.Services;
+using RocketseatAuction.API.UseCases.Offers.CreateOffer;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(option =>
+{
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    { 
+        Description = @"JW Authorization header using TagHelperServicesExtensions Bearer scheme.
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      Exemple: 'Bearer 1234abcdef'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"                        
+    });
+
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement 
+    {
+        { 
+            new OpenApiSecurityScheme
+            { 
+                Reference = new OpenApiReference
+                { 
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }        
+    });
+});
+
+builder.Services.AddScoped<AuthenticationUserAttribute>();
+builder.Services.AddScoped<LoggedUser>();
+builder.Services.AddScoped<CreateOfferUserCase>();
+
+builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
